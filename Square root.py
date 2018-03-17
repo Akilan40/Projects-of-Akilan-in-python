006bf2998277e2c405fdd26587aa8c6f36f5533d
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-03-17 10:29:55 +0530

    Add files via upload

---
+++ Square root.py
@@ -0,0 +1,6 @@
+print("\t\t\t\t\tFinding square root")
+a=0
+n=float(input("Enter the number that you want to find the root of:"))
+a=n**0.5
+print("The square root of {0} is :{1}".format(n,a))
+print("\n\n\t\tThank you") 
