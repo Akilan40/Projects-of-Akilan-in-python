94936dee9d81ca39e40709f640faad67479488e7
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-03-19 12:30:14 +0530

    Add files via upload

---
+++ numbers 2.py
@@ -0,0 +1,8 @@
+import math
+a=10.45
+b=-67.34
+print("abs of [10.45,-67.34]:{0},{1}".format(abs(a),abs(b)))
+print("ceil of 10.45:",math.ceil(10.45))
+print("ceil of -67.45:",math.ceil(-67.34))
+print("max of 10.45,-67.34:",max(10.45,-67.34))
+print("min of 10.45,-67.34:",min(10.45,-67.34))
