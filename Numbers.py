cb8080692414288df76fb1d49f886d530e149357
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-03-20 11:25:34 +0530

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -1,10 +1,10 @@
 print("\n\n\t\t\tNumbers")
 a=10.456
-print("\n\n\t Value of a:{0}".format(a))
+print("\n\n\t Value of <a>:{0}".format(a))
 b=int(a)
-print("\n\t a as integer value:{0}".format(b))
+print("\n\t <a> as integer value:{0}".format(b))
 b=float(a)
-print("\n\t a as a float value:{0}".format(b))
+print("\n\t <a> as a float value:{0}".format(b))
 b=complex(a)
-print("\n\t a as a complex value:{0}".format(b))
+print("\n\t <a> as a complex value:{0}".format(b))
 
