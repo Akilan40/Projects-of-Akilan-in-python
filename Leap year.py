1670ed4c3fecc13e3c95212630e1ec0e07cf31e9
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-03-26 19:20:35 +0530

    Add files via upload

---
+++ Leap year.py
@@ -0,0 +1,12 @@
+print("\n\n\n\t\t\t\tLeap year")
+year=int(input("Enter the year: "))
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
