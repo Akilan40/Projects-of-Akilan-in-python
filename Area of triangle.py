9fa3df4cd3963bd2219790c14d1275d3b1fee4c4
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-04-03 11:59:38 +0530

    Add files via upload

---
+++ Area of triangle.py
@@ -0,0 +1,5 @@
+print'\n\n\n\t\t\t\t\t\tArea of a tringle'
+a=float(input('Enter the base length: '))
+b=float(input('Enter the heigt of the triangle: '))
+c=0.5*a*b
+print'The area o the triangle is: ',c
