6319f7afed4955679f07b8182c72953ce900c3c4
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-04-04 12:28:11 +0530

    Add files via upload

---
+++ checking for +,-,0.py
@@ -0,0 +1,8 @@
+print'\n\n\n\n\t\t\t\t\t\t\t\tChecking for +,-,0'
+a=int(input('\n\nEnter the number: '))
+if a<0:
+    print'The number entered is a negative number'
+if a>0:
+    print'The number entered is a positive number'
+if a==0:
+    print'The number entered is 0'
