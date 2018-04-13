b6be7b623a7604ca8e5481bee1b729aaa904ab91
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-04-13 11:03:19 +0530

    Update Io error.py

--- Ioerror/Io error.py
+++ Ioerror/Io error.py
@@ -1,6 +1,7 @@
 try:
     fh=open("Test file",'w')
-    fh.write('This is test file')
+    fh.write('This was created by Akilan ,so say thanks to Akilan')
+    fh.write('\n\nThank you Akilan :)')
 except IOError:
     print'Error:can/t ind file or read data'
 else:
