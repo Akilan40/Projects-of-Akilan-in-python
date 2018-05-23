670eef9f8018e99350348bf0f627900bac568add
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2018-05-23 10:15:11 +0530

    Update celcius to farenheat.py

--- celcius to farenheat.py
+++ celcius to farenheat.py
@@ -1,4 +1,4 @@
 print("\n\n\t\t\t\tCelcius to farenheit")
 c=float(input("Enter celcius:"))
 f=(c*1.8)+32
-print("{0} celcius is equal to {1} farenheit".format(c,f))
+print(c,'celcius is equal to' ,f,'farenheit')
