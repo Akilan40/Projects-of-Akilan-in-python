6ce2797bab5dddd937b9d55b7cf2aee7cbeb1a3a
Akilan40 <37430857+Akilan40@users.noreply.github.com>
2019-05-04 12:38:41 +0530

    Update updating list.py

--- updating list.py
+++ updating list.py
@@ -1,5 +1,11 @@
 print("\n\n\t\t\tUpdating a list")
 list=['phy','chem',1,2]
+c=list
+print("List before update",c)
 print("value available at index 2:",list[2])
 list[2]=2;
-print("New value available at index 2:",list[2])
+print("New value at index 2:",list[2])
+b=len(list)
+print("len list:",b)
+a=list
+print("list after Updating:",a)
